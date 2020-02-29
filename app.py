5fa06ae696d9b69e8c660fcdbd5a17bb3391f22f
Fernando XPS <ferna.zepeda@gmail.com>
2020-02-29 21:37:42 +0100

    last changes

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
-from dash.dependencies import Output, Input, State
+from dash.dependencies import Output, Input
 from dash import Dash
 import sqlalchemy as db
 import pandas as pd
@@ -13,141 +13,173 @@ import plotly.graph_objects as go
 from datetime import datetime
 from datetime import timedelta
 from collections import deque
-import cat_modules
+import cat_modules as cats
 import dash_bootstrap_components as dbc
+import numpy as np
+import sqlite3
 
+# Global variables
+standard_date_format = "%Y-%m-%d %H:%M:%S"
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app: Dash = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-server = app.server
-
-# Open connection to read cat_meals db
-engine = db.create_engine('sqlite:///catmeals.sqlite', echo=False)
-sql_DF = pd.read_sql("SELECT * FROM cat_meals", con=engine)
-sql_DF = sql_DF.drop('index', axis=1)
-
-
-# Check the time format
-sql_DF['next_meals'] = pd.to_datetime(sql_DF['next_meals'], format="%Y-%m-%d %H:%M:%S")
-print(sql_DF)
 
 # Style variables
 colors = {'background': '#FFFFFF', 'text': '#111111', 'general': 'RoyalBlue'}
 
-fig = go.Figure()
-fig.add_trace(go.Scatter(x=sql_DF['next_meals'],
-                         y=sql_DF['remaining_meals'],
-                         name="Remaining cat meals",
-                         opacity=1.0,
-                         marker=dict(size=30,
-                                     line=dict(color="black"),
-                                     symbol='x'),
-                         mode='markers',
-                         # visible='legendonly',
-                         hovertemplate="date: %{x}<br>remaining meals: %{y}<extra></extra>"))
+# SQL lite URI
+db_uri = "sqlite:///catmeals.sqlite"
 
-fig.update_layout(xaxis_range=[datetime.now() - timedelta(days=1),
-                               datetime.now() + timedelta(days=1)],
-                  title_text="",
-                  template="plotly_white")
+app: Dash = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 
-fig.add_shape(
-    dict(
-        type="line",
-        x0=datetime.now(),
-        y0=0,
-        x1=datetime.now(),
-        y1=4.5,
-        line=dict(
-            color=colors['general'],
-            width=3
-        )
+
+# Open connection to read cat_meals db
+def get_data(target_db_uri):
+    engine = db.create_engine(target_db_uri, echo=False)
+    tmp_table = pd.read_sql("SELECT * FROM cat_meals", con=engine)
+    tmp_table = tmp_table.drop('index', axis=1)
+    # Check the time format
+    print("[STATUS] Actual future meal records:")
+    print(tmp_table)
+    return tmp_table
+
+
+def generate_plot(data_frame_input):
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data_frame_input['next_meals'],
+                             y=data_frame_input['remaining_meals'],
+                             name="Remaining cat meals",
+                             marker=dict(size=30,
+                                         line=dict(color="black"),
+                                         symbol='x'),
+                             mode='markers',
+                             hovertemplate="date: %{x}<br>remaining meals: %{y}<extra></extra>"))
+
+    fig.update_layout(xaxis_range=[datetime.now() - timedelta(days=1),
+                                   datetime.now() + timedelta(days=1)],
+                      title_text="",
+                      template="plotly_white")
+
+    fig.add_shape(
+        dict(type="line",
+             x0=datetime.now(),
+             y0=0,
+             x1=datetime.now(),
+             y1=4.5,
+             line=dict(color=colors['general'], width=3)
+             ))
+
+    fig.add_trace(go.Scatter(
+        x=[datetime.now().strftime("%Y-%m-%d %H:%M")],
+        y=[-0.5],
+        text=["Now"],
+        mode="text",
+        showlegend=False
     ))
 
-fig.add_trace(go.Scatter(
-    x=[datetime.now().strftime("%Y-%m-%d %H:%M")],
-    y=[-0.5],
-    text=["Now"],
-    mode="text",
-    showlegend=False
-))
+    return fig
 
+
+# Get data from sqllite
+sql_df = get_data(db_uri)
 
 # Define app layout
 app.layout = html.Div([
-        # Title
-        html.H1(
-            children='Catmate Tracker',
-            style={
-                'textAlign': 'center',
-                'color': colors['text']
-            }
-        ),
-        dcc.Markdown('''
+
+    # Title
+    html.H1(
+        children='Catmate Tracker',
+        style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }
+    ),
+
+    dcc.Markdown('''
             A web application for tracking the [catmate](https://pet-mate.com/gb/product/c300-automatic-pet-feeder-with-digital-timer/) status
             
         ''',
-                     style={'textAlign': 'center'}),
-        # Update button container
-        html.Div(html.Button('Update!',
-                             id='button',
-                             style={'textAlign': 'center'}),
-                 style={'textAlign': 'center'}
-                 ),
-        # Update button container
-        html.Div(id='output-container-button',
-                 children='Press Update! after filling the catmate...',
                  style={'textAlign': 'center'}),
-        html.Br(),
-        html.Div(
-            children=[dash_table.DataTable(
-                id='table',
-                columns=[{"name": i, "id": i, "selectable": True} for i in sql_DF.columns],
-                data=sql_DF.to_dict('records'),
-                editable=False,
-                style_as_list_view=True,
-                style_cell={'padding': '5px', 'textAlign': 'left'},
-                style_cell_conditional=[{'if': {'column_id': 'remaining_meals'}, 'textAlign': 'center'}],
-                sort_action="native",
-                page_action="native",
-                style_header={
-                    'backgroundColor': 'rgb(230, 230, 230)',
-                    'fontWeight': 'bold'
-                },
-                style_data_conditional=[{
-                    "if": {'column_id': str(x),
-                           # create the filter query JS + python. Really important: use the JS time format
-                           'filter_query': '{next_meals} > ' + datetime.now().strftime("%Y-%m-%d") +
-                                           'T' + datetime.now().strftime("%H:%M:%S")
-                           },
-                    "backgroundColor": colors['general'],
-                    'color': 'white'} for x in sql_DF.columns
-                ]
-            )],
-            className='row'
-        ),
 
-        html.Br(),
-        html.Div(id='live-update-text'),
-        dcc.Graph(id='live-graph', animate=True, figure=fig),
-        dcc.Interval(id='interval-component', interval=30 * 1000, n_intervals=0),
-        html.Hr(),
-        dcc.Markdown(f'''v0.1
+    # Update button container
+    html.Div(html.Button('Update!',
+                         id='button',
+                         style={'textAlign': 'center'}),
+             style={'textAlign': 'center'}
+             ),
+
+    # Update button container
+    html.Div(id='output-container-button',
+             children='Press Update! after filling the catmate...',
+             style={'textAlign': 'center'}),
+
+    html.Div(id="recent-update", style={'textAlign': 'center'}),
+    html.Br(),
+
+    html.Div(
+        children=[dash_table.DataTable(
+            id='table',
+            columns=[{"name": i, "id": i, "selectable": True} for i in sql_df.columns],
+            data=sql_df.to_dict('records'),
+            editable=False,
+            style_as_list_view=True,
+            style_cell={'padding': '5px', 'textAlign': 'left'},
+            style_cell_conditional=[{'if': {'column_id': 'remaining_meals'}, 'textAlign': 'center'}],
+            sort_action="native",
+            page_action="native",
+            style_header={
+                'backgroundColor': 'rgb(230, 230, 230)',
+                'fontWeight': 'bold'
+            },
+            style_data_conditional=[{
+                "if": {'column_id': str(x),
+                       # create the filter query JS + python. Really important: use the JS time format
+                       'filter_query': '{next_meals} > ' + datetime.now().strftime("%Y-%m-%d") +
+                                       'T' + datetime.now().strftime("%H:%M:%S")
+                       },
+                "backgroundColor": colors['general'],
+                'color': 'white'} for x in sql_df.columns
+            ]
+        )],
+        className='row'
+    ),
+
+    html.Br(),
+
+    html.Div(id='live-update-text'),
+
+    dcc.Graph(id='live-graph', animate=True, figure=generate_plot(sql_df)),
+
+    html.Div(id="new-records", style={'textAlign': 'center'}),
+
+    # Interval component: serves just to refresh the application
+
+    dcc.Interval(id='interval-component', interval=30 * 1000, n_intervals=0),
+
+    html.Hr(),
+
+    dcc.Markdown(f'''v0.1
 {datetime.now().strftime("%B %Y")}
 
 Check the project in [GitHub/Fmrhj](https://github.com/Fmrhj/catmate-tracker) 
 ''',
-                     style={'textAlign': 'center'})
-    ])
+                 style={'textAlign': 'center'})
+])
 
-#app.layout = serve_layout
+
+# app.layout = serve_layout
+
 
 # Button update
 @app.callback(
     Output('output-container-button', 'children'),
     [Input('button', 'n_clicks')])
 def update_output(n_clicks):
-    return 'The button has been clicked {} times'.format(n_clicks)
+    msg = 'The button has been clicked {} times'.format(n_clicks)
+    if n_clicks is not None:
+        tmp = "Table has been successfully updated!"
+        msg = 'The button has been clicked {} times'.format(n_clicks) + '. ' + tmp
+    return msg
+
 
 # Actual time
 @app.callback(Output('live-update-text', 'children'),
@@ -157,6 +189,43 @@ def update_metrics(n):
     return [
         html.Span(f'Actual Time: {datetime.now().strftime("%Y-%m-%d %H:%M")}', style=style),
     ]
+
+
+@app.callback(
+    Output("new-records", "children"),
+    [Input("button", "n_clicks")],
+)
+def add_and_show_records(n):
+    if n is not None:
+        # Create new pandas data frame with future meals
+        new_records = cats.generate_next_meals_table()
+        print("[UPDATE] New meals!")
+        print(new_records)
+
+        # Open connection
+        con = sqlite3.connect("catmeals.sqlite")
+
+        # Write results
+        new_records.to_sql("cat_meals", con, if_exists="replace")
+
+        # Close connection
+        con.close()
+
+    return html.Span("")
+
+
+@app.callback(
+    Output("recent-update", "children"),
+    [Input('interval-component', 'n_intervals')])
+def check_recent_update(n):
+    tmp_check = get_data(db_uri)
+    last_value = tmp_check['time_stamp'][0]
+    msg = ""
+    # Check if the table has been update recently, i.e. in the last 2 hours
+    if datetime.now() < datetime.strptime(last_value, standard_date_format) + timedelta(hours=2):
+        msg = "The records have been updated recently"
+    return msg
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- cat_modules.py
+++ cat_modules.py
@@ -1,7 +1,8 @@
 from datetime import datetime
+from datetime import timedelta
 import numpy as np
+import pandas as pd
 
-# Global variable
 standard_date_format = "%Y-%m-%d %H:%M:%S"
 
 def perdelta(start, end, delta):
@@ -11,27 +12,47 @@ def perdelta(start, end, delta):
         yield curr
         curr += delta
 
+
 def nearest(items, pivot):
     # calculates the nearest item in a list from a pivot item
     return min(items, key=lambda x: abs(x - pivot))
 
 
-""" Function to fill a table with future meal time stamps
-Args: 
-    time_stamp_now (datetime)
+def generate_next_meals_table():
+    """ Function to fill a table with future meal time stamps
+     Args:
 
-Returns:
-    list: with date for next meals
-"""
-def generateNextMeals(time_stamp_now=datetime.now()):
+     Returns:
+         tmp_frame (DataFrame): pandas DataFrame with future meals
+     """
+    time_stamp_var = datetime.now()
+    tmp = {'time_stamp': time_stamp_var.strftime(standard_date_format),
+           'next_meals': generate_next_meals(time_stamp_var),
+           'remaining_meals': np.linspace(start=4, stop=1, num=4)}
+    tmp_frame = pd.DataFrame(tmp)
+
+    return tmp_frame
+
+
+def generate_next_meals(time_stamp_now=datetime.now()):
+    """ Function to fill a table with future meal time stamps
+    Args:
+        time_stamp_now (datetime)
+
+    Returns:
+        list: with date for next meals
+    """
     today_evening = datetime.strptime(time_stamp_now.strftime("%Y-%m-%d") + " 19:00:00", standard_date_format)
     today_morning = datetime.strptime(time_stamp_now.strftime("%Y-%m-%d") + " 07:00:00", standard_date_format)
 
-    # Assign next meal to next following date
-    if (today_evening > time_stamp_now):
-        next_meal = today_evening
-    else:
-        next_meal = today_morning
+    # Assign next meal to next following date today
+    next_meal = max(today_evening, today_morning)
+
+    # If the next meal is already in the past, take the next meal in the morning
+    if next_meal < time_stamp_now:
+        tomorrow_morning = datetime.strptime(time_stamp_now.strftime("%Y-%m-%d") + " 07:00:00",
+                                             standard_date_format) + timedelta(days=1)
+        next_meal = tomorrow_morning
 
     # Generate following meals for the next days.
     # Our catmate is programmed to rotate every 12 hours with 4 slots = 2 days
@@ -39,16 +60,16 @@ def generateNextMeals(time_stamp_now=datetime.now()):
     for date_item in perdelta(next_meal, next_meal + timedelta(days=2), timedelta(days=0.5)):
         date_list.append(date_item)
 
-    return (date_list)
+    return date_list
 
 
-""" Function to retrieve how many meals are left
-Args:
-    meals_table (DataFrame) pandas DataFrame
+def calculate_remaining_meals(meals_table):
+    """ Function to retrieve how many meals are left
+    Args:
+        meals_table (DataFrame): pandas DataFrame
 
-Returns:
-    remaining_meals (int) 
-"""
-def calculateRemainingMeals(meals_table):
-    b = a[a.next_meals == nearest(a['next_meals'], datetime.now())][['remaining_meals']]
-    return (b.remaining_meals)
+    Returns:
+        remaining_meals (int): number of remaining meals
+    """
+    b = meals_table[meals_table.next_meals == nearest(meals_table['next_meals'], datetime.now())][['remaining_meals']]
+    return b.remaining_meals
